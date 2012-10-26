360085fa3f36de7312272ca6f5dc60d0c7258e6f
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2012-10-26 21:08:37 +0100

    Add main view (basic/flat version)

---
+++ application/views/main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>#MAZZOPS</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/public.css">
+
+  </head>
+  <body>
+
+    <h1>Is MAZZOPS happening?</h1>
+    <h2 class="notnow">Not right now</h2>
+
+    <p class="otter"><img src="/images/mazzops-otter.png" width="200px"/></p>
+    <p class="builtfor">Built for <a href="http://twitter.com/mnowster">@mnowster</a> by <a href="http://twitter.com/dafyddbach">@dafyddbach</a>.
+      <br/>Otter drawn by <a href="http://twitter.com/markhurrell">@markhurrell</a> for  <a href="http://twitter.com/mnowster">@mnowster</a>.</p>
+
+  </body>
+</html>
